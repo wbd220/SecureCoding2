be30f4092f5e124c7aff7ef514ed05e790ce40df
wbd220@nyu.edu <Cyb3rc0d3cl4ss!>
2019-10-12 16:04:59 -0400

    strip end of line from two-factor input

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def register():
     if register_form.validate_on_submit():
         if register_form.username.data not in userdict:
             userdict[register_form.username.data] = {'password': register_form.password.data,
-                                                     '2fa': register_form.two_fa_field.data}
+                                                     '2fa': register_form.two_fa_field.data.strip('\n')}
             flash(f"Registration successful for user {register_form.username.data} Please login")
             return render_template('register.html', form=register_form, success='success')
         else:
