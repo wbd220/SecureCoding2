294b6fc3d2e56e70c60c6f60292ce03037a16593
wbd220@nyu.edu <Cyb3rc0d3cl4ss!>
2019-10-17 00:33:28 -0400

    adjust the data in the reqdata dictionary before asserting them to the request command

--- Test_for_service.py
+++ Test_for_service.py
@@ -24,7 +24,7 @@ class FeatureTest(unittest.TestCase):
         print("testing to see if /register page is there")
 
     def test_register_account(self):
-        reqdata = {"tester2": uname, "password": pword, "15553334444": two_fa_field}
+        reqdata = {"uname": "tester2", "pword": "password", "2fa": "15553334444"}
         req = requests.post(server_address + "/register", data=reqdata)
         print(req)
         soup = BeautifulSoup(req.text, "html_parser")
