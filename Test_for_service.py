259e887dd33d139e752527f4776012ef2a8afa59
wbd220@nyu.edu <Cyb3rc0d3cl4ss!>
2019-10-16 22:47:15 -0400

    print something out when doing your testing to know that tests are running

--- Test_for_service.py
+++ Test_for_service.py
@@ -10,11 +10,14 @@ class FeatureTest(unittest.TestCase):
     def home_page_check(self):
         req = requests.get(server_address)
         self.assertEqual(req.status_code, 200)
+        print("testing to see if there is a default page")
 
     def login_page_check(self):
         req = requests.get(server_address + "/login")
         self.assertEqual(req.status_code, 200)
+        print("testing to see if /login page is there")
 
     def register_page_check(self):
         req = requests.get(server_address + "/register")
         self.assertEqual(req.status_code, 200)
+        print("testing to see if /register page is there")
