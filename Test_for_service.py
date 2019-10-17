f068c0214ea7e4ea6eec25932e2fd8ad3b2ebfea
wbd220@nyu.edu <Cyb3rc0d3cl4ss!>
2019-10-17 00:22:36 -0400

    added a login test (that I hope works!)

--- Test_for_service.py
+++ Test_for_service.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+from bs4 import BeautifulSoup
 
 server_address = "http://127.0.0.1:5000"
 SERVICE_ADDR = server_address
@@ -21,6 +22,15 @@ class FeatureTest(unittest.TestCase):
         req = requests.get(server_address + "/register")
         self.assertEqual(req.status_code, 200)
         print("testing to see if /register page is there")
+
+    def test_register_account(self):
+        reqdata = {"tester2": uname, "password": pword, "15553334444": 2fa}
+        req = requests.post(server_address + "/register", data=reqdata)
+        print(req)
+        soup = BeautifulSoup(req.text, "html_parser")
+        login_result=soup.find("result")
+        self.assertEqual(login_result, "success")
+
 
 
 if __name__ == '__main__':
