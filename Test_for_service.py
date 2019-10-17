776b95a16aeb60921d4fe09748338d5e8e268c5d
wbd220@nyu.edu <Cyb3rc0d3cl4ss!>
2019-10-16 23:18:49 -0400

    put test in front of the call message definitions

--- Test_for_service.py
+++ Test_for_service.py
@@ -7,17 +7,17 @@ SERVICE_ADDR = server_address
 
 class FeatureTest(unittest.TestCase):
 
-    def home_page_check(self):
+    def test_home_page_check(self):
         req = requests.get(server_address)
         self.assertEqual(req.status_code, 200)
         print("testing to see if there is a default page")
 
-    def login_page_check(self):
+    def test_login_page_check(self):
         req = requests.get(server_address + "/login")
         self.assertEqual(req.status_code, 200)
         print("testing to see if /login page is there")
 
-    def register_page_check(self):
+    def test_register_page_check(self):
         req = requests.get(server_address + "/register")
         self.assertEqual(req.status_code, 200)
         print("testing to see if /register page is there")
